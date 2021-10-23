bbb69030236f1258b170e5dc6451dd6c05018baa
alberto <medina.v@pm.me>
2021-10-23 19:39:28 +0100

    added stactic files and new migration for users to enter upload images

--- projects/forms.py
+++ projects/forms.py
@@ -5,4 +5,4 @@ class ProjectForm(ModelForm):
     #generator form based on Project modeo
     class Meta:
         model = Project
-        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
