f15fe0a7e4e538d668eba5df5c210a9d1413d00b
alberto <medina.v@pm.me>
2021-10-24 18:41:57 +0100

    applied theme in the form and single project page

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,20 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
-    #generator form based on Project modeo
+    #generates based on project model
     class Meta:
         model = Project
-        fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'featured_image', 'description',
+                  'demo_link', 'source_link']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    #allows ud to classes to input fields
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
