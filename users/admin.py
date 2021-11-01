65824c167d6f3509748efa9ea15706adeef8686d
alberto <medina.v@pm.me>
2021-11-01 21:58:08 +0000

    added message functionality

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Profile, Skill
+from .models import Profile, Profile, Skill, Message
 admin.site.register(Profile)
 admin.site.register(Skill)
+admin.site.register(Message)
